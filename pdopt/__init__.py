1e7eb4d3d9fc19800cad95c8ce99ae5f207f3f7f
Andrea S <spina95@gmail.com>
2024-01-11 12:33:03 +0000

    Created src folder

--- pdopt/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 15 11:28:15 2021
-
-@author: Andrea Spinelli
-"""
-
-
---
+++ src/pdopt/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 11:28:15 2021
+
+@author: Andrea Spinelli
+"""
+
+__author__ = 'Andrea Spinelli'
+__copyright__ = 'Copyright 2021, all rights reserved'
+__status__ = 'Development'
+__version__ = 0.4.0
