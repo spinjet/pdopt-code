31f65670a6fa449833f975c7cba305950c74c2ad
Andrea S <spina95@gmail.com>
2024-01-24 11:23:57 +0000

    Fixed FutureWarning in examples, added helper methods for I/O in DesignSpace and ProbabilistcExploration
    - Fixed the FutureWarning error in the example files as discussed in https://github.com/spinjet/pdopt-code/issues/3
    - Added pickle load/save interface for both DesignSpace and ProbabilisticExploration objects
    - Added a .csv saving interface in DesignSpace for saving the results
    - Added a parameters sampling interface to DesignSpace in case user wishes to sample designs from any sets (improves usability).
    
    Examples have been updated with the new interfaces and linted with `black`.

--- example/example_1/energy_management_experiment.py
+++ example/example_1/energy_management_experiment.py
@@ -54,271 +54,257 @@ import numpy as np
 from tqdm import tqdm
 
 from pdopt.data import DesignSpace, ExtendableModel
-from pdopt.exploration import  ProbabilisticExploration
+from pdopt.exploration import ProbabilisticExploration
 from pdopt.optimisation import Optimisation
 from pdopt.tools import generate_run_report
-#from pdopt.visualisation import main_inline
+
+# from pdopt.visualisation import main_inline
 
 from HE_Model import model, postpro_run
 
 
 class Experiment(ExtendableModel):
-    
     def __init__(self, input_parameters, architecture, mission_file):
-        self.inp        = list(pd.read_csv(input_parameters)['name'])
-        self.arch       = architecture
-        self.arch0      = architecture
-        self.mission    = mission_file
-        
+        self.inp = list(pd.read_csv(input_parameters)["name"])
+        self.arch = architecture
+        self.arch0 = architecture
+        self.mission = mission_file
+
     def run(self, *args, **kwargs):
         # The input of the model is variable
         # we need to construct the energy management dataframe
-        
+
         X, parms = [], []
         en_mgm = []
-        
+
         # The TO/LND conditions will be single point only
         # logic to convert TO/LND into takeoff, climbout, final, landing
         for i in range(len(self.inp)):
+            if "TO" in self.inp[i]:
+                parms.append(self.inp[i].replace("TO", "takeoff"))
+                parms.append(self.inp[i].replace("TO", "climbout"))
+                X.append(args[i])
+                X.append(args[i])
 
-            if 'TO' in self.inp[i]:
-                parms.append(self.inp[i].replace('TO','takeoff'))
-                parms.append(self.inp[i].replace('TO','climbout'))
+            elif "LND" in self.inp[i]:
+                parms.append(self.inp[i].replace("LND", "final"))
+                X.append(args[i])
+
+            elif "LNDToAlternate" in self.inp[i]:
+                parms.append(self.inp[i].replace("LND", "final"))
+                parms.append(self.inp[i].replace("LNDToAlternate", "landing"))
                 X.append(args[i])
                 X.append(args[i])
-                
-            elif 'LND' in self.inp[i]:
-                parms.append(self.inp[i].replace('LND','final'))
-                X.append(args[i])
-                
-            elif 'LNDToAlternate' in self.inp[i]:
-                parms.append(self.inp[i].replace('LND','final'))
-                parms.append(self.inp[i].replace('LNDToAlternate','landing'))
-                X.append(args[i])
-                X.append(args[i])
-            
+
             else:
                 parms.append(self.inp[i])
                 X.append(args[i])
-                
-        
+
         # Stuff to introduce constraints over the x_positions
         all_x_vals = []
         x_vals = []
         old_seg = None
-        
+
         for i in range(len(parms)):
             # separate segment from type
-            segment, value = parms[i].split('_')
-            
+            segment, value = parms[i].split("_")
+
             if old_seg != segment and len(x_vals) > 0:
                 all_x_vals.append(x_vals)
 
             x_vals = [] if old_seg != segment else x_vals
 
-            if value[0] == 'h':
+            if value[0] == "h":
                 if len(en_mgm) == 0 or en_mgm[-1][0] != segment:
                     en_mgm.append([segment, 0, X[i]])
                 else:
                     en_mgm.append([segment, 1, X[i]])
-            elif value[0] == 'x':
+            elif value[0] == "x":
                 x_vals.append(X[i])
                 en_mgm[-1][1] = X[i]
-                
+
             old_seg = segment
         all_x_vals.append(x_vals)
 
-        
-        en_management = pd.DataFrame(en_mgm, columns=['segment','x','doh'])
-        
-        #For doing UQ on architecture parameters
+        en_management = pd.DataFrame(en_mgm, columns=["segment", "x", "doh"])
+
+        # For doing UQ on architecture parameters
         checks = [
-            'e_bat' in kwargs.keys(),
-            'motor' in kwargs.keys(),
-            'power_el' in kwargs.keys(),
-            'cables' in kwargs.keys(),
-            'battery' in kwargs.keys()
-            ]
-        
+            "e_bat" in kwargs.keys(),
+            "motor" in kwargs.keys(),
+            "power_el" in kwargs.keys(),
+            "cables" in kwargs.keys(),
+            "battery" in kwargs.keys(),
+        ]
+
         if any(checks):
-            
-            if 'e_bat' in kwargs.keys():
-                self.arch['e_bat'] = kwargs['e_bat']
-                
-            if 'motor' in kwargs.keys():
-                self.arch['eta_e_comp']['motor'] = kwargs['motor']
-                self.arch['eta_e'] = np.prod(
-                    list(self.arch['eta_e_comp'].values())
-                    )
-            
-            if 'power_el' in kwargs.keys():
-                self.arch['eta_e_comp']['power_el'] = kwargs['power_el']
-                self.arch['eta_e'] = np.prod(
-                    list(self.arch['eta_e_comp'].values())
-                    )
-                
-            if 'cables' in kwargs.keys():
-                self.arch['eta_e_comp']['cables'] = kwargs['cables']
-                self.arch['eta_e'] = np.prod(
-                    list(self.arch['eta_e_comp'].values())
-                    )
-        
-            if 'battery' in kwargs.keys():
-                self.arch['eta_e_comp']['battery'] = kwargs['battery']
-                self.arch['eta_e'] = np.prod(
-                    list(self.arch['eta_e_comp'].values())
-                    )
-            
+            if "e_bat" in kwargs.keys():
+                self.arch["e_bat"] = kwargs["e_bat"]
+
+            if "motor" in kwargs.keys():
+                self.arch["eta_e_comp"]["motor"] = kwargs["motor"]
+                self.arch["eta_e"] = np.prod(list(self.arch["eta_e_comp"].values()))
+
+            if "power_el" in kwargs.keys():
+                self.arch["eta_e_comp"]["power_el"] = kwargs["power_el"]
+                self.arch["eta_e"] = np.prod(list(self.arch["eta_e_comp"].values()))
+
+            if "cables" in kwargs.keys():
+                self.arch["eta_e_comp"]["cables"] = kwargs["cables"]
+                self.arch["eta_e"] = np.prod(list(self.arch["eta_e_comp"].values()))
+
+            if "battery" in kwargs.keys():
+                self.arch["eta_e_comp"]["battery"] = kwargs["battery"]
+                self.arch["eta_e"] = np.prod(list(self.arch["eta_e_comp"].values()))
+
         else:
             self.arch = self.arch0.copy()
-        
-        
-            
-        
-        
-        analysis = model(en_management, architecture_data=self.arch, mission_file=self.mission)
-        
+
+        analysis = model(
+            en_management, architecture_data=self.arch, mission_file=self.mission
+        )
+
         output = {
-                'TOM'      : analysis.iloc[-1].mass,
-                'Mf'       : analysis.iloc[-1].m_fl,
-                'M_NOx'    : analysis.iloc[-1].m_NOx,
-                 }
-        
+            "TOM": analysis.iloc[-1].mass,
+            "Mf": analysis.iloc[-1].m_fl,
+            "M_NOx": analysis.iloc[-1].m_NOx,
+        }
+
         # Add constraints over the position of the segments, x2 - x1 < 0
         if len(all_x_vals) > 0:
             counter = 1
             for x_vals in all_x_vals:
-                for i in range(1,len(x_vals)):
-                    output.update({f'x{counter}':x_vals[i-1]-x_vals[i]})
+                for i in range(1, len(x_vals)):
+                    output.update({f"x{counter}": x_vals[i - 1] - x_vals[i]})
                     counter += 1
-                
+
         return output
-    
+
     def postprocess_analysis(self, *args, **kwargs):
         # The input of the model is variable
         # we need to construct the energy management dataframe
-        
+
         X, parms = [], []
         en_mgm = []
-        
+
         # The TO/LND conditions will be single point only
         # logic to convert TO/LND into takeoff, climbout, final, landing
         for i in range(len(self.inp)):
-            if 'TO' in self.inp[i]:
-                parms.append(self.inp[i].replace('TO','takeoff'))
-                parms.append(self.inp[i].replace('TO','climbout'))
+            if "TO" in self.inp[i]:
+                parms.append(self.inp[i].replace("TO", "takeoff"))
+                parms.append(self.inp[i].replace("TO", "climbout"))
                 X.append(args[i])
                 X.append(args[i])
-                
-            elif 'LND' in self.inp[i]:
-                parms.append(self.inp[i].replace('LND','final'))
+
+            elif "LND" in self.inp[i]:
+                parms.append(self.inp[i].replace("LND", "final"))
                 X.append(args[i])
-                
-            elif 'LNDToAlternate' in self.inp[i]:
-                parms.append(self.inp[i].replace('LND','final'))
-                parms.append(self.inp[i].replace('LNDToAlternate','landing'))
+
+            elif "LNDToAlternate" in self.inp[i]:
+                parms.append(self.inp[i].replace("LND", "final"))
+                parms.append(self.inp[i].replace("LNDToAlternate", "landing"))
                 X.append(args[i])
                 X.append(args[i])
-            
+
             else:
                 parms.append(self.inp[i])
                 X.append(args[i])
-                
-        
+
         for i in range(len(parms)):
             # separate segment from type
-            segment, value = parms[i].split('_')
-            
-            if value[0] == 'h':
+            segment, value = parms[i].split("_")
+
+            if value[0] == "h":
                 if len(en_mgm) == 0 or en_mgm[-1][0] != segment:
                     en_mgm.append([segment, 0, X[i]])
                 else:
                     en_mgm.append([segment, 1, X[i]])
-            elif value[0] == 'x':
+            elif value[0] == "x":
                 en_mgm[-1][1] = X[i]
 
-        
-        en_management = pd.DataFrame(en_mgm, columns=['segment','x','doh'])
-        
-        #For doing UQ on battery energy density
-        if 'e_bat' in kwargs.keys():
-            self.arch['e_bat'] = kwargs['e_bat']
+        en_management = pd.DataFrame(en_mgm, columns=["segment", "x", "doh"])
+
+        # For doing UQ on battery energy density
+        if "e_bat" in kwargs.keys():
+            self.arch["e_bat"] = kwargs["e_bat"]
         else:
             self.arch = self.arch0.copy()
-        
-        
-        analysis = model(en_management, architecture_data=self.arch, mission_file=self.mission)
-        
+
+        analysis = model(
+            en_management, architecture_data=self.arch, mission_file=self.mission
+        )
+
         results = postpro_run(self.arch, analysis)
 
         return results
-    
-    
+
+
 def run_experiment(folder, n_exp_samples, P_exploration, restart, n_exp_train):
-    print('Input Args: ', folder, n_exp_samples, P_exploration, restart, n_exp_train)
-    
-    architecture = json.load(open('data/architecture.json', 'r'))
-    mission = 'data/mission_original.csv'
-    experiment = Experiment(folder + '/input.csv', architecture, mission)
-    
-    
+    print("Input Args: ", folder, n_exp_samples, P_exploration, restart, n_exp_train)
+
+    architecture = json.load(open("data/architecture.json", "r"))
+    mission = "data/mission_original.csv"
+    experiment = Experiment(folder + "/input.csv", architecture, mission)
+
     # Check if a design space is already present otherwise create it
-    if exists(folder + '/design_space.pk') and restart:
-        design_space = pk.load(open(folder + '/design_space.pk','rb'))
+    if exists(folder + "/design_space.pk") and restart:
+        design_space = DesignSpace.from_pickle(folder + "/design_space.pk")
     else:
-        design_space = DesignSpace.from_csv(folder + '/input.csv', folder + '/response.csv')
-        pk.dump(design_space, open(folder + '/design_space.pk','wb'))
-        
+        design_space = DesignSpace.from_csv(
+            folder + "/input.csv", folder + "/response.csv"
+        )
+        design_space.save_to_pickle(folder + "/design_space.pk")
+
     # Check if there is already a trained exploration object
-    if exists(folder + '/exploration.pk') and restart:
-        exploration = pk.load(open(folder + '/exploration.pk','rb'))
+    if exists(folder + "/exploration.pk") and restart:
+        exploration = ProbabilisticExploration.from_pickle(folder + "/exploration.pk")
     else:
-        exploration = ProbabilisticExploration(design_space, experiment,
-                                           surrogate_training_data_file=folder + '/samples.csv',
-                                           n_train_points=n_exp_train)
-        
+        exploration = ProbabilisticExploration(
+            design_space,
+            experiment,
+            surrogate_training_data_file=folder + "/samples.csv",
+            n_train_points=n_exp_train,
+        )
+
         for k in exploration.surrogates:
             s = exploration.surrogates[k]
-            print(f'Surrogate {s.name} with r = {s.score:.4f}')
-    
-        pk.dump(exploration, open(folder + '/exploration.pk','wb'))
-    
-    
+            print(f"Surrogate {s.name} with r = {s.score:.4f}")
+
+        exploration.save_to_pickle(folder + "/exploration.pk")
+
     # Check if exploration has been done already
-    if exists(folder + '/exp_results.csv') and restart:
+    if exists(folder + "/exp_results.csv") and restart:
         pass
     else:
         exploration.run(n_exp_samples, P_exploration)
-        design_space.get_exploration_results().to_csv(folder + '/exp_results.csv', index=False)
-        
-        #Update the saved design object
-        pk.dump(design_space, open(folder + '/design_space.pk','wb'))
-    
-    
-    optimisation = Optimisation(design_space, experiment, n_max_evals=2000,
-                                use_surrogate=False)
+        design_space.save_exploration_results(folder + "/exp_results.csv")
 
+        # Update the saved design object
+        design_space.save_to_pickle(folder + "/design_space.pk")
+
+    optimisation = Optimisation(
+        design_space, experiment, n_max_evals=2000, use_surrogate=False
+    )
 
     # Check if optimisation has been done already
-    if exists(folder + '/opt_results.csv') and restart:
+    if exists(folder + "/opt_results.csv") and restart:
         pass
     else:
         optimisation.run(folder)
-        df_opt = design_space.get_optimum_results()
-        df_opt.to_csv(folder + '/opt_results.csv', index=False)
-        
-        #Update the saved design object
-        pk.dump(design_space, open(folder + '/design_space.pk','wb'))
+        design_space.save_optimisation_results(folder + "/opt_results.csv")
+
+        # Update the saved design object
+        design_space.save_to_pickle(folder + "/design_space.pk")
+
+    # Runtime Report
+    generate_run_report(folder + "/report.txt", design_space, optimisation, exploration)
 
 
-    #Runtime Report
-    generate_run_report(folder + '/report.txt', design_space, optimisation, exploration)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # run experiment with the input set inside the file
     case_folder = "test_case_linear"
     P_sat = 0.5
-    n_exp_samples   = 100
+    n_exp_samples = 100
     n_train_samples = 100
     restart = False
 
