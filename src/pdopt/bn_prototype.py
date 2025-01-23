2769752935e66fcdc17867ad487e2e1f3d50ea66
Andrea S. <spina95@gmail.com>
2025-01-23 16:10:18 +0000

    Update bn_prototype.py
    Sketch code for the BN module update

--- src/pdopt/bn_prototype.py
+++ src/pdopt/bn_prototype.py
@@ -10,6 +10,10 @@ import pdopt.exploration as exploration
 import pandas as pd
 import numpy as np
 
+from pgmpy.inference import VariableElimination, BeliefPropagation
+from pgmpy.models import BayesianNetwork
+from pgmpy.utils import get_example_model
+from pgmpy.estimators import MaximumLikelihoodEstimator
 
 #from pdopt.data import DesignSpace, ExtendableModel, ContinousParameter
 
@@ -45,8 +49,8 @@ obj  = [val['obj'] for val in test_out]
 con1 = [val['con1'] for val in test_out]
 con2 = [val['con2'] for val in test_out] 
 
-## method for discretisation
-class BNUniformDiscretiser:
+## object for discretisation
+class UniformDiscretiser1D:
     def __init__(self, data, n_levels):
         
         self.n_levels = n_levels
@@ -54,13 +58,41 @@ class BNUniformDiscretiser:
         
         self.level_width  = (x.max() - x.min())/self.n_levels
         self.split_points = [
-                x.min() + self.level_width * (n + 1) for n in range(self.num_buckets - 1)
+                x.min() + self.level_width * (n + 1) for n in range(self.n_levels - 1)
             ]
     
     def transform(self, data):
         return np.digitize(data, self.split_points)
         
         
+## object for mapping 
+    
+    
+
+
+
+## BN Representation Class
+earthquake = get_example_model('earthquake')
+samples = earthquake.simulate(n_samples=100)
+
+class BayesianNetworkModel:
+    def __init__(self, structure_dict):
+        self._bn = BayesianNetwork(structure_dict)
+        self.nodes = self._bn.nodes()
+        self.edges = self._bn.edges()
+    
+    def fit_sates(self, df_data):
+        pass
+    
+
+# Initialize the estimator object.
+bn = BayesianNetwork(earthquake.edges())
+mle_est = MaximumLikelihoodEstimator(model=bn, data=samples)
+all_cpds = mle_est.get_parameters(n_jobs=1)
+
+
+## Extension of Parameter classes to support BN sampling/discretisation
+
+## Exploration class using BN
+
 ## 
-    
-    
